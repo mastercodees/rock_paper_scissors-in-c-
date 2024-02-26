239e57eb20d112e731d877302b4d87b4e6b0e04a
Vladmir Nyataige <124368658+mastercodees@users.noreply.github.com>
2024-02-27 00:27:36 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+
+
+     class Program
+    {
+        public static void Main()
+        {
+            string inputPlayer;
+            string inputCPU;
+            int randomInt;
+            int scorePlayer = 0;
+            int scoreCPU = 0;
+            
+            Console.WriteLine("Choose between ROCK, PAPER and SCISSORS");
+            inputPlayer = Console.ReadLine();
+            Random random_number = new Random();
+
+            randomInt = random_number.Next(1, 4);
+
+            switch (randomInt)
+            {
+                case 1:
+                    inputCPU = "ROCK";
+                    Console.WriteLine("Computer chose ROCK");
+                    if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("DRAW");
+                    }
+                    else if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("player WINS!");
+                        scorePlayer++;
+                    }
+                    else if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("CPU WINS!");
+                        scoreCPU++; 
+                    }
+                        
+                    break;
+                case 2:
+                    inputCPU = "PAPER";
+                    
+                    Console.WriteLine("Computer chose PAPER");
+                    if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("DRAW");
+                    }
+                    else if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("CPU WINS!");
+                        scoreCPU++;
+                    }
+                    else if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("player WINS!");
+                        scorePlayer++; 
+                    }
+
+                    break;
+                case 3:
+                    inputCPU = "SCISSORS";
+                    Console.WriteLine("Computer chose SCISSORS");
+                    if (inputPlayer == "SCISSORS")
+                    {
+                        Console.WriteLine("DRAW");
+                    }
+                    else if (inputPlayer == "PAPER")
+                    {
+                        Console.WriteLine("CPU WINS!");
+                        scoreCPU++;
+                    }
+                    else if (inputPlayer == "ROCK")
+                    {
+                        Console.WriteLine("PLAYER WINS!");
+                        scorePlayer++; 
+                    }
+
+                    break;
+                    
+                default:
+                    Console.WriteLine("Invalid entry!");
+                    break;
+                        
+                
+            }
+
+        }
+    }
